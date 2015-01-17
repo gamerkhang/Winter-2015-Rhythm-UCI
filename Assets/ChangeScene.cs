f73fadb06731c06f1e6e34c8fb30b02b3a78bd03
Christopher Luong <bumblefo@gmail.com>
2015-01-16 23:09:04 -0800

    Added in all scenes and ChangeScene.cs

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Update is called once per frame
+	public void ChangeToScene (int sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
