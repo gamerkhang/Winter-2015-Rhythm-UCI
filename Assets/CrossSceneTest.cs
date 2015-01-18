6d556d69aad97d4084e1f94c6d3151d0f136348f
Christopher Luong <bumblefo@gmail.com>
2015-01-18 13:02:02 -0800

    removed song on play from CrossSceneTest

--- Assets/CrossSceneTest.cs
+++ Assets/CrossSceneTest.cs
@@ -9,8 +9,6 @@ public class CrossSceneTest : MonoBehaviour {
 	void Start () {
 		test.text = "" + ApplicationModel.P2Char;
 		TEST2.text = ApplicationModel.songbeatmap;
-		GameObject.Find ("GameManager").audio.clip = ApplicationModel.songaudiofile;
-		GameObject.Find ("GameManager").audio.Play ();
 	}
 	
 	// Update is called once per frame
